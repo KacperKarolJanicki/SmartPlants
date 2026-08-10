988b057fe706447916ee0b7b87377f25ffae427f
Kacper Janicki <kacperkaroljanicki@gmail.com>
2026-08-10 16:12:01 +0200

    Merge to main

--- python_backend/app.py
+++ python_backend/app.py
@@ -5,47 +5,57 @@ from database import *
 from ai_logic import ai
 from manual_logic import *
 from datetime import *
+import threading
 
 load_dotenv()
 app = Flask(__name__)
 
+
 @app.route("/ground_data", methods=["GET","POST"])
 def ground_data():
+    # authorization()
+
+    device_ip = request.headers.get('deviceIp')
+
     if request.method == "GET":
-        response = requests.get("http://192.168.0.26")
+        response = requests.get(device_ip)
         return response.json()
     if request.method == "POST":
         data = request.get_json()
-        response = requests.post("http://192.168.0.26", json=data)
+        response = requests.post(device_ip, json=data)
         return response.json()
 
 
 @app.route("/light", methods=["POST","GET"])
 def light():
+
+    device_ip = request.headers.get('deviceIp')
+
     if request.method == "GET":
         result = manual_steering.light_steering()
         return result
     if request.method == "POST":
+        # authorization()
         data = request.get_json()
-        response = requests.post("http://192.168.0.27", json=data)
+        response = requests.post(device_ip, json=data)
         return "Ok"
-    
-@app.route("/plants_data", methods=["POST","GET"])
-def plants_data():
-    header = request.headers.get("deviceId")
 
-    if request.method == "GET":
-        authorization(header)
-        return list(plants_database[header].find({},{"_id":0}))[::-1]
-    if request.method == "POST":
-        authorization(header)
-        data = request.get_json()
-        if header not in plants_database.list_collection_names():
-            plants_database.create_collection(header)
-            plants_database[header].insert_one(data)
-        else:
-            plants_database[header].insert_one(data)
-        return "Data updated"
+@app.route("/automatics", methods=["POST"])
+def automatics():
+    device_ip = request.headers.get('deviceIp')
+    data = request.get_json()
+    print(data)
+    try:
+        requests.post(f"{device_ip}/automatics", json=data)
+        return "Ok"
+    except Exception as e:
+        return e 
+    
+@app.route("/plants_data", methods=["GET"])
+def plants_data():
+    # header = request.headers.get("deviceId")
+    # authorization(header)
+    return list(plants_database["data"].find({},{"_id":0}))[::-1]
         
         
 
@@ -53,4 +63,5 @@ def plants_data():
 def home():
     return "Connected"
 
+threading.Thread(target=run).start()
 app.run(host="0.0.0.0", port=3350, debug=False)
--- python_backend/database.py
+++ python_backend/database.py
@@ -1,5 +1,21 @@
 from pymongo import *
-
+import requests
+import time
+from datetime import datetime, timezone
 
 client = MongoClient("mongodb://localhost:27017/")
 plants_database = client["plants_data"]
+
+def ask_sensors():
+    data = {"datetime": datetime.now(timezone.utc).isoformat()}
+    try:
+        response = requests.get("http://192.168.0.26/")
+        data.update(response.json())
+        return data
+    except Exception as e:
+        return {"error": e}
+
+def run():
+    while True:
+        plants_database["data"].insert_one(ask_sensors())
+        time.sleep(3600)
